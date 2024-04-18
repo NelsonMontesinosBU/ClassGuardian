cbdce15396e86abd2e4a431f7985ddb1e193cfa5
NelsonMontesinosBU <79678727+NelsonMontesinosBU@users.noreply.github.com>
2024-04-17 22:16:10 -0500

    create 2fa controller

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -10,3 +10,33 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+const { Injectable } = require('@nestjs/common');
+const speakeasy = require('speakeasy');
+
+@Injectable()
+class TwoFactorAuthenticationService {
+
+  generateTwoFactorAuthenticationSecret(username) {
+    const secret = speakeasy.generateSecret({
+      name: `ByteScrum Custom App:${username}`,
+    });
+    return secret.base32;
+  }
+
+  generateTwoFactorAuthenticationToken(secret) {
+    return speakeasy.totp({
+      secret,
+      encoding: 'base32',
+    });
+  }
+  validateTwoFactorAuthenticationToken(token, secret) {
+    return speakeasy.totp.verify({
+      secret,
+      encoding: 'base32',
+      token,
+      window: 1, 
+  }
+}
+
+module.exports = TwoFactorAuthenticationService;
