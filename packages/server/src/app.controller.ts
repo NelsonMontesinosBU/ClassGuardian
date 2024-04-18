61da462cae5560ad79f3868b9411e7e6416cdb4b
NelsonMontesinosBU <79678727+NelsonMontesinosBU@users.noreply.github.com>
2024-04-17 22:30:16 -0500

    Generate 2FA secret

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -40,3 +40,55 @@ class TwoFactorAuthenticationService {
 }
 
 module.exports = TwoFactorAuthenticationService;
+
+const { Controller, Get, Req, Res } = require('@nestjs/common');
+const { TwoFactorAuthenticationService } = require('./two-factor-authentication.service');
+const speakeasy = require('speakeasy');
+const QRCode = require('qrcode');
+
+@Controller('2fa')
+class TwoFactorAuthenticationController {
+  constructor(twoFactorAuthenticationService) {
+    this.twoFactorAuthenticationService = twoFactorAuthenticationService;
+  }
+
+  @Get('generate')
+  async generateTwoFactorAuth(req, res) {
+
+    const user = req.user;
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not authenticated' });
+    }
+
+
+    if (user.isTwoFactorAuthenticationEnabled) {
+      return res.status(400).json({ message: '2FA already enabled!' });
+    }
+
+
+    const secret = this.twoFactorAuthenticationService.generateTwoFactorAuthenticationSecret(user.username);
+
+
+    const otpAuthUrl = speakeasy.otpauthURL({
+      secret: secret,
+      label: `YourApp:${user.username}`,
+      issuer: 'YourApp', 
+    });
+
+    try {
+
+      const qrCodeDataURL = await QRCode.toDataURL(otpAuthUrl);
+
+
+      return res.status(200).json({ qrCode: qrCodeDataURL });
+    } catch (error) {
+
+      console.error('Error generating QR code:', error);
+      return res.status(500).json({ message: 'Error generating QR code' });
+    }
+  }
+}
+
+module.exports = TwoFactorAuthenticationController;
+
